d6b6bafe8f7c1e30dfbc34d0977246ef36e5b54e
bakha <djickel.diaw@systematix-qc.com>
2023-11-14 21:49:43 -0500

    add changes to the test

---
+++ test_main.py
@@ -0,0 +1,14 @@
+import unittest
+from main import reverse_list
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_reverse_list(self):
+        data = [1,3,5]
+        data_reversed = [5,3,1]
+        self.assertEqual(reverse_list(data), data_reversed)
+
+if __name__ == '__main__':
+    unittest.main()
